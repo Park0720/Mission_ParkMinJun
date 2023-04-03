f561c45258ed0041ac35d7dc2663b3a749cee0f0
Park0720 <ppmm98@naver.com>
2023-04-03 15:31:33 +0900

    setting

---
+++ src/main/java/com/example/mission_parkminjun/MissionParkMinJunApplication.java
@@ -0,0 +1,13 @@
+package com.example.mission_parkminjun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionParkMinJunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MissionParkMinJunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mission_parkminjun/MissionParkMinJunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mission_parkminjun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionParkMinJunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
