718bf9fc97843903b5fed87e16bdf740995f5be9
Park0720 <ppmm98@naver.com>
2023-04-26 16:19:10 +0900

    if you insert InstaId at link, get userinfo

--- src/main/java/com/ll/gramgram/boundedContext/home/controller/HomeController.java
+++ src/main/java/com/ll/gramgram/boundedContext/home/controller/HomeController.java
@@ -1,23 +1,33 @@
 package com.ll.gramgram.boundedContext.home.controller;
 
 import com.ll.gramgram.base.rq.Rq;
+import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
+import com.ll.gramgram.boundedContext.instaMember.service.InstaMemberService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Enumeration;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
     private final Rq rq;
-
+    private final InstaMemberService instaMemberService;
     @GetMapping("/")
     public String showMain() {
         return "usr/home/main";
+    }
+    @GetMapping("/{instaID}")
+    public String showMember(@PathVariable String instaID) {
+        InstaMember instaMember = instaMemberService.findByUsername(instaID).orElseThrow();
+        return "usr/instaMember/memberinfo";
     }
 
     @GetMapping("/usr/debugSession")
